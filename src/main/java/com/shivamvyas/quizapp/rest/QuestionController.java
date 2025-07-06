d4c30ac8d90d924a27b6fc68d9761cee6b5c8a94
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-07-06 11:20:22 +0530

    Add QuestionController to handle requests.

---
+++ src/main/java/com/shivamvyas/quizapp/rest/QuestionController.java
@@ -0,0 +1,30 @@
+package com.shivamvyas.quizapp.rest;
+
+import com.shivamvyas.quizapp.model.Question;
+import com.shivamvyas.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("/question")
+public class QuestionController {
+
+    private QuestionService service;
+
+    @Autowired
+    public void setService(QuestionService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/all-questions")
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        List<Question> questions = service.getQuestions();
+        return new ResponseEntity<List<Question>>(questions, HttpStatus.OK);
+    }
+
+}
