2f2fd31e0a7b3b280961cf68e35e9387ead2cf15
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-07-06 16:55:52 +0530

    Add calculateResult method to IQuizService for result computation

--- src/main/java/com/shivamvyas/quizapp/service/IQuizService.java
+++ src/main/java/com/shivamvyas/quizapp/service/IQuizService.java
@@ -1,9 +1,11 @@
 package com.shivamvyas.quizapp.service;
 
 import com.shivamvyas.quizapp.model.QuestionClient;
+import com.shivamvyas.quizapp.model.Response;
 import java.util.List;
 
 public interface IQuizService {
     String createQuiz(String category, int numOfQues, String title);
     List<QuestionClient> getQuizQuestions(Long id);
+    String calculateResult(Long id, List<Response> responses);
 }
