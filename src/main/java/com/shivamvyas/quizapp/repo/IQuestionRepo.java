6a7caea1543ef251e93feb8cca307de73fab077d
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-07-06 14:59:27 +0530

    Add findRandomQuesByCategory() for retrieving random questions by category

--- src/main/java/com/shivamvyas/quizapp/repo/IQuestionRepo.java
+++ src/main/java/com/shivamvyas/quizapp/repo/IQuestionRepo.java
@@ -3,6 +3,7 @@ package com.shivamvyas.quizapp.repo;
 import com.shivamvyas.quizapp.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -15,4 +16,9 @@ public interface IQuestionRepo extends JpaRepository<Question, Long> {
 
     //show questions by category
     List<Question> findByCategory(String category);
+
+    //getting random questions by category as per specific numbers
+    @Query(value = "SELECT * FROM Question q WHERE q.category= :category ORDER BY RAND() LIMIT :num", nativeQuery = true)
+    List<Question> findRandomQuesByCategory(@Param("category") String category, @Param("num") int num);
+
 }
