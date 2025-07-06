9aa9f0c5b74f5f4e604015e897a773f221aaf031
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-07-06 16:54:59 +0530

    Add Response model for handling quiz responses

---
+++ src/main/java/com/shivamvyas/quizapp/model/Response.java
@@ -0,0 +1,13 @@
+package com.shivamvyas.quizapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Response {
+    private Long id;
+    private String response;
+}
