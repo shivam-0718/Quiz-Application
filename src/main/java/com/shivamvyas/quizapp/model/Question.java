2569865b1e3580797f68cafa349a6164ffb2d75c
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-07-06 11:16:18 +0530

    Add Question entity with attributes for quiz application

---
+++ src/main/java/com/shivamvyas/quizapp/model/Question.java
@@ -0,0 +1,43 @@
+package com.shivamvyas.quizapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "questionTitle")
+    private String questionTitle;
+
+    @Column(name = "option1")
+    private String option1;
+
+    @Column(name = "option2")
+    private String option2;
+
+    @Column(name = "option3")
+    private String option3;
+
+    @Column(name = "option4")
+    private String option4;
+
+    @Column(name = "rightAnswer")
+    private String rightAnswer;
+
+    @Column(name = "difficultyLevel")
+    private String difficultyLevel;
+
+    @Column(name = "category")
+    private String category;
+
+}
