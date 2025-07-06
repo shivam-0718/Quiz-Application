ba77fcfd37c9974a4fe6445b7fc2bab755948588
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-07-06 13:21:35 +0530

    Add methods for adding, retrieving, and updating questions

--- src/main/java/com/shivamvyas/quizapp/service/IQuestionService.java
+++ src/main/java/com/shivamvyas/quizapp/service/IQuestionService.java
@@ -1,5 +1,6 @@
 package com.shivamvyas.quizapp.service;
 
+import com.shivamvyas.quizapp.exception.QuestionNotFoundException;
 import com.shivamvyas.quizapp.model.Question;
 import java.util.List;
 
@@ -7,4 +8,7 @@ public interface IQuestionService {
     List<Question> getQuestions();
     List<String> showCategoriesList();
     List<Question> getQuestionsByCategory(String category);
+    String addQuestion(Question question);
+    Question getQuestionById(Long id) throws QuestionNotFoundException;
+    String updateQuestion(Long id, Question question) throws QuestionNotFoundException;
 }
