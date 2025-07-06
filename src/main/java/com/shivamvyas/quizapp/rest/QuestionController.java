4c629f3281b3b57f83448587e4c5c0212e2aa682
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-07-06 11:56:41 +0530

    Add methods to retrieve question categories and questions by category.

--- src/main/java/com/shivamvyas/quizapp/rest/QuestionController.java
+++ src/main/java/com/shivamvyas/quizapp/rest/QuestionController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -25,6 +26,19 @@ public class QuestionController {
     public ResponseEntity<List<Question>> getAllQuestions() {
         List<Question> questions = service.getQuestions();
         return new ResponseEntity<List<Question>>(questions, HttpStatus.OK);
+    }
+
+    @GetMapping("/category")
+    public ResponseEntity<List<String>> showCategories() {
+        List<String> categories = service.showCategoriesList();
+        return new ResponseEntity<List<String>>(categories, HttpStatus.OK);
+    }
+
+    @GetMapping("/category/{category}")
+    public ResponseEntity<List<Question>> getQuestionByCategory(@PathVariable("category") String category) {
+        List<Question> questions = service.getQuestionsByCategory(category);
+        return new ResponseEntity<List<Question>>(questions, HttpStatus.OK);
+
     }
 
 }
