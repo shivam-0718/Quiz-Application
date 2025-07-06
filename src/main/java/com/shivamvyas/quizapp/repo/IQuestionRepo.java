9e2dd143fe03aab886e7c61a4ca5c2f281fe85be
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-07-06 11:56:00 +0530

    Add showCategories method to IQuestionRepo for retrieving distinct question categories

--- src/main/java/com/shivamvyas/quizapp/repo/IQuestionRepo.java
+++ src/main/java/com/shivamvyas/quizapp/repo/IQuestionRepo.java
@@ -2,8 +2,15 @@ package com.shivamvyas.quizapp.repo;
 
 import com.shivamvyas.quizapp.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface IQuestionRepo extends JpaRepository<Question, Long> {
+
+    //show categories first
+
+    @Query("SELECT DISTINCT q.category FROM Question q")
+    List<String> showCategories();
 }
