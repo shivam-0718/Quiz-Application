16c121c19eb8af4fce80f62ceaef02e0e457cbcf
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-07-06 13:20:17 +0530

    Add ErrorDetails class for structured error response handling

---
+++ src/main/java/com/shivamvyas/quizapp/advice/ErrorDetails.java
@@ -0,0 +1,15 @@
+package com.shivamvyas.quizapp.advice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDetails {
+    private String message;
+    private LocalDateTime dateTime;
+}
