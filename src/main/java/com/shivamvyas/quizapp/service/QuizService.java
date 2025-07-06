42e42cd4c0dfaefe04fb88124e2e3c3bf81249a8
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-07-06 15:42:55 +0530

    Add getQuizQuestions method to QuizService for retrieving quiz questions

--- src/main/java/com/shivamvyas/quizapp/service/QuizService.java
+++ src/main/java/com/shivamvyas/quizapp/service/QuizService.java
@@ -1,12 +1,16 @@
 package com.shivamvyas.quizapp.service;
 
+import com.shivamvyas.quizapp.exception.QuizNotAvailableException;
 import com.shivamvyas.quizapp.model.Question;
+import com.shivamvyas.quizapp.model.QuestionClient;
 import com.shivamvyas.quizapp.model.Quiz;
 import com.shivamvyas.quizapp.repo.IQuestionRepo;
 import com.shivamvyas.quizapp.repo.IQuizRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService implements IQuizService{
@@ -28,5 +32,33 @@ public class QuizService implements IQuizService{
         repo.save(quiz);
 
         return "Quiz has been created!";
+    }
+
+    public List<QuestionClient> getQuizQuestions(Long id) {
+        Optional<Quiz> optional = repo.findById(id);
+        if(optional.isPresent()) {
+            Quiz quizDb = optional.get();
+            List<Question> questionsFromDb = quizDb.getQuestions();
+            return getQuestionClients(questionsFromDb);
+        }
+        throw new QuizNotAvailableException("Quiz with given id is not available. Please try again!");
+    }
+
+    private static List<QuestionClient> getQuestionClients(List<Question> questionsFromDb) {
+        List<QuestionClient> questionsForUsers = new ArrayList<>();
+
+        for(Question q : questionsFromDb) {
+            QuestionClient qc = new QuestionClient();
+
+            qc.setId(q.getId());
+            qc.setQuestionTitle(q.getQuestionTitle());
+            qc.setOption1(q.getOption1());
+            qc.setOption2(q.getOption2());
+            qc.setOption3(q.getOption3());
+            qc.setOption4(q.getOption4());
+
+            questionsForUsers.add(qc);
+        }
+        return questionsForUsers;
     }
 }
