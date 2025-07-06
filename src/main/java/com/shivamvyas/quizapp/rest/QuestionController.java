5ae3333b01023e7eb8c6c59661f934733c057759
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-07-06 13:22:26 +0530

    Refactor QuestionController to streamline endpoint definitions and improve readability

--- src/main/java/com/shivamvyas/quizapp/rest/QuestionController.java
+++ src/main/java/com/shivamvyas/quizapp/rest/QuestionController.java
@@ -5,10 +5,7 @@ import com.shivamvyas.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
@@ -38,7 +35,19 @@ public class QuestionController {
     public ResponseEntity<List<Question>> getQuestionByCategory(@PathVariable("category") String category) {
         List<Question> questions = service.getQuestionsByCategory(category);
         return new ResponseEntity<List<Question>>(questions, HttpStatus.OK);
-
     }
+
+    @PostMapping("/add-question")
+    public ResponseEntity<String> addAQuestion(@RequestBody Question question) {
+        String response = service.addQuestion(question);
+        return new ResponseEntity<String>(response, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update-question/{id}")
+    public ResponseEntity<String> updateAQuestion(@PathVariable("id")Long id, @RequestBody Question question) {
+        String response = service.updateQuestion(id, question);
+        return new ResponseEntity<String>(response, HttpStatus.OK);
+    }
+
 
 }
