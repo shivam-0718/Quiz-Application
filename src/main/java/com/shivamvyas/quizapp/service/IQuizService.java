810986c55e1f396e8e36e942f2242c4675c8792c
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-07-06 15:42:49 +0530

    Add getQuizQuestions method to IQuizService

--- src/main/java/com/shivamvyas/quizapp/service/IQuizService.java
+++ src/main/java/com/shivamvyas/quizapp/service/IQuizService.java
@@ -1,5 +1,9 @@
 package com.shivamvyas.quizapp.service;
 
+import com.shivamvyas.quizapp.model.QuestionClient;
+import java.util.List;
+
 public interface IQuizService {
-    String createQuiz(String category, int numOfOues, String title);
+    String createQuiz(String category, int numOfQues, String title);
+    List<QuestionClient> getQuizQuestions(Long id);
 }
